d4beda3eab4550fab678a28d33440dbe82dcce98
Daniel Liu <zhanliu324@gmail.com>
2021-08-17 18:23:05 -0700

    js fundamentals

---
+++ 1.Web-Fundamentals/3.JS/6.JavaScript-Fundamentals/loop-challenges.js
@@ -0,0 +1,25 @@
+for (var i=1; i<=20; i++) {
+    if (i%2==1) {
+        console.log(i);
+    }
+}
+
+for (var i=100; i>=0; i-=3) {
+    console.log(i);
+}
+
+for (var i=4; i>=-3.5; i-=1.5) {
+    console.log(i);
+}
+
+var sum = 0
+for (var i=1; i<=100; i++) {
+    sum += i;
+}
+console.log(sum)
+
+var product = 1
+for (var i=1; i<=12; i++) {
+    product *= i;
+}
+console.log(product)
