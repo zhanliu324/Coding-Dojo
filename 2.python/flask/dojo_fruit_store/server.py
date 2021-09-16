a56fdf8ec975bf656a766c0cdab4aa0773e0a6bb
Daniel Liu <zhanliu324@gmail.com>
2021-09-15 21:21:55 -0700

    upload fruit store

---
+++ 2.python/flask/dojo_fruit_store/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    print('Charging ' + request.form['first_name'] + ' ' + request.form['last_name'] + ' for ' + str(int(request.form['strawberry'])+int(request.form['raspberry'])+int(request.form['apple'])) + ' fruits')
+    return render_template('checkout.html')
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
