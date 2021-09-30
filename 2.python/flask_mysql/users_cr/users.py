e45e2e524dba2896cb5fac3599b2ac83e2d1cd2b
Daniel Liu <zhanliu324@gmail.com>
2021-09-29 21:52:21 -0700

    upload user cr

---
+++ 2.python/flask_mysql/users_cr/users.py
@@ -0,0 +1,28 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the user table from our database
+
+class User:
+    def __init__(self, data) -> None:
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "select * from users;"
+        results = connectToMySQL('users_schema').query_db(query)
+        users = []
+        for result in results:
+            users.append(cls(result))
+        return users
+
+    @classmethod
+    def save(cls, data):
+        query = "insert into users (first_name, last_name, email, created_at, updated_at) values (%(first_name)s, %(last_name)s, %(email)s, now(), now());"
+        # comes back as the new row id
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return result
